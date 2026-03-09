733c8b030ca4f8868f51ffcd349742769c710b95
hojen2 <hoj2@oregonstate.edu>
2026-03-09 08:52:23 -0700

    Fixed 'credits' to 'courseCredits'

--- app.js
+++ app.js
@@ -310,8 +310,8 @@ app.post('/courses/create', async (req, res) => {
         const { courseCode, courseTitle, courseCredit, departmentID } = req.body;
 
         await db.query(
-            `INSERT INTO Courses (courseCode, courseTitle, credits, departmentID) VALUES (?, ?, ?, ?)`,
-            [courseCode, courseTitle, credits, departmentID]
+            `INSERT INTO Courses (courseCode, courseTitle, courseCredits, departmentID) VALUES (?, ?, ?, ?)`,
+            [courseCode, courseTitle, courseCredits, departmentID]
         );
 
         res.redirect('/courses');
@@ -326,10 +326,10 @@ app.post('/courses/create', async (req, res) => {
 // =====================
 app.post('/courses/update', async (req, res) => {
     try {
-        const { courseID, courseTitle, credits, departmentID } = req.body;
+        const { courseID, courseTitle, courseCredits, departmentID } = req.body;
         await db.query(
-            `UPDATE Courses SET courseCode = ?, courseTitle = ?, credits = ?, departmentID = ? WHERE courseID = ?`,
-            [courseCode, courseTitle, credits, departmentID, courseID]
+            `UPDATE Courses SET courseCode = ?, courseTitle = ?, courseCredits = ?, departmentID = ? WHERE courseID = ?`,
+            [courseCode, courseTitle, courseCredits, departmentID, courseID]
         );
         res.redirect('/courses');
     } catch (err) {
